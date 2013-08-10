022aae346d05312301bdc51c5e268ea48ca19002
liwei606@gmail.com <liwei606@gmail.com>
2013-08-10 16:48:21 +0800

    add divide two integers

---
+++ leetcode/divide_two_integers.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    int divide(int dividend, int divisor) {
+        // Start typing your C/C++ solution below
+        // DO NOT write int main() function
+        int sign = 1;
+        if (divisor < 0 ^ dividend < 0) {
+            sign = -1;
+        }
+        long long d1 = dividend;
+        long long d2 = divisor;
+        d1 = abs(d1);
+        d2 = abs(d2);
+        long long ans = 0;
+        long long p = 1;
+        while (d1 >= d2) {
+            p = p << 1;
+            d2 = d2 << 1;
+        }
+        while (p > 1) {
+            p = p >> 1;
+            d2 = d2 >> 1;
+            if (d2 <= d1) {
+                d1 -= d2;
+                ans += p;
+            }
+        }
+        return ans * sign;
+    }
+};
