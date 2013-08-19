5d62bf1da7c5c02ba5752cfc7499d9e3b666ca71
liwei606@gmail.com <liwei606@gmail.com>
2013-08-19 22:14:59 +0800

    add search insert position

---
+++ leetcode/search_insert_position.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int searchInsert(int A[], int n, int target) {
+        // Start typing your C/C++ solution below
+        // DO NOT write int main() function
+        int l = 0, r = n - 1;
+        while (l <= r) {
+            int m = (l + r) / 2;
+            if (A[m] == target) return m;
+            else if (A[m] > target) r = m - 1;
+            else l = m + 1;
+        }
+        return l;
+    }
+};
